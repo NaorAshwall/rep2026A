208c7f64e48eda7552708eb996b6096f689a6628
shoafi1000 <shoafi1000@gmail.com>
2026-03-26 12:31:32 +0200

    3b

--- rep2026A-version1/rep2026A-version1/ThreadCheckArray/ThreadCheckArray/src/SharedData.java
+++ rep2026A-version1/rep2026A-version1/ThreadCheckArray/ThreadCheckArray/src/SharedData.java
@@ -1,43 +1,71 @@
 import java.util.ArrayList;
-
+/**
+ * 
+ */
+//
 public class SharedData 
 {
+	/**
+	 * 
+	 */
 	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
-	
+	/**
+	 * 
+	 * @param array
+	 * @param b
+	 */
 	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
-
+	/**
+	 * 
+	 * @param winArray
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public int getB() 
 	{
 		return b;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
-
+	/**
+	 * 
+	 * @param flag
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
